c75d24c3654d09b1028c5184bbde78d7dfdff26e
Praixx <praix!y@gmail.com>
2023-12-21 22:05:22 +0100

    first commirrt

--- script.js
+++ script.js
@@ -6,13 +6,19 @@ menuHmaburger.addEventListener("click", menuAnimation);
 function menuAnimation(event) {
     event.preventDefault();
 
-    if (menuNav.style.right === "0px") {
-        // menuNav.style.display = "none" 
-        menuNav.style.right = "-300px";
+    // if (menuNav.style.right === "0px") { 
+    //     menuNav.style.right = "-300px";
+    //     menuNav.style.display = "none"
         
-    } else {
-        // menuNav.style.display = "block"
-        menuNav.style.right = "0px"; 
+    // } else {
+    //     //menuNav.style.display = "block"
+    //     menuNav.style.right = "0px"; 
         
+    // }
+
+    if(menuNav.style.display === "none"){
+        menuNav.style.display = "block"
+    }else{
+        menuNav.style.display = "none"
     }
 }
